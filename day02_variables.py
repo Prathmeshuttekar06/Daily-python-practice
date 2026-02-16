72dc9e357678e720a0b08d93ac5838206cefb8d6
Prathmesh <uttekarprathmesh30@gmail.com>
2026-02-16 12:17:54 +0530

    variables and types of variables

--- day02_variables.py
+++ day02_variables.py
@@ -1,4 +1,5 @@
 #creating variables
+""" 
 name="Prathmesh"
 age=22
 height=5.8
@@ -6,4 +7,23 @@ is_student=True
 print("Name:", name)
 print("Age:", age)  
 print("Height:", height)
+print("Is Student:", is_student) 
+"""
+#Integer
+age=22
+#Float
+height=5.8
+#String
+name="Prathmesh"
+#Boolean
+is_student=True
+print("Name:", name)
+print("Age:", age)  
+print("Height:", height)
 print("Is Student:", is_student)
+print("name type:", type(name))
+print("age type:", type(age))
+print("height type:", type(height))
+print("is_student type:", type(is_student))
+
+
