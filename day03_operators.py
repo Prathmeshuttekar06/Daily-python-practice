555a0aa1e27dc3c83955a94c55fa1c2a256363ff
Prathmesh <uttekarprathmesh30@gmail.com>
2026-02-17 21:12:40 +0530

    Day 03 - Operators Practice

---
+++ day03_operators.py
@@ -0,0 +1,24 @@
+#Day 03 - Operators Practice
+#Arithmetic Operators
+a=10
+b=3
+print("addition:", a+b)
+print("subtraction:", a-b)
+print("multiplication:", a*b)
+print("division:", a/b)
+print("modulus:", a%b)
+print("exponentiation:", a**b)
+print("-------------------------")
+#Comparison Operators
+print("Is a equal to b?", a==b)
+print("Is a not equal to b?", a!=b)
+print("Is a greater than b?", a>b)
+print("Is a less than b?", a<b)
+print("Is a greater than or equal to b?", a>=b)
+print("--------------------------")
+#Logical Operators
+x=True
+y=False
+print("x and y:", x and y)
+print("x or y:", x or y)
+print("not x:", not x)
