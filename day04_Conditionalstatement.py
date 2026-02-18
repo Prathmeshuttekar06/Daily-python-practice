e63ed78aeda790fc74d1e0ce83d9ba9c9898205b
Prathmesh <uttekarprathmesh30@gmail.com>
2026-02-18 19:22:45 +0530

    Conditional Statement

---
+++ day04_Conditionalstatement.py
@@ -0,0 +1,24 @@
+#Day 04 - Conditional Statements 
+#simple if-else statement
+age=18
+
+if age>=18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+print("-------------------------")
+#if-elif-else statement
+marks=int(input("Enter your marks: "))
+if marks>=90:
+    print("Grade: A")
+elif marks>=80:
+    print("Grade: B")
+elif marks>=70:
+    print("Grade: C")
+elif marks>=60:
+    print("Grade: D")
+else:    print("Grade: F")
+print("-------------------------")
+
+
