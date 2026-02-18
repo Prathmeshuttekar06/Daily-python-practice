09050d92dac5c90f20f59ce768bc6b9e29055008
Prathmesh <uttekarprathmesh30@gmail.com>
2026-02-18 19:36:33 +0530

    conditional statement and example

--- day04_Conditionalstatement.py
+++ day04_Conditionalstatement.py
@@ -9,7 +9,7 @@ else:
 
 print("-------------------------")
 #if-elif-else statement
-marks=int(input("Enter your marks: "))
+marks=85
 if marks>=90:
     print("Grade: A")
 elif marks>=80:
@@ -20,5 +20,40 @@ elif marks>=60:
     print("Grade: D")
 else:    print("Grade: F")
 print("-------------------------")
+#Nested if statement
+num=10
+if num>0:
+    print("The number is positive.")
+    if num%2==0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+print("-------------------------")
 
-
+#Example 1 : Check Positive, Negative or Zero
+number = -5
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+print("-------------------------")
+#Example 2 : find the largest of three numbers
+num1 = 10
+num2 = 20
+num3 = 15
+if num1 >= num2 and num1 >= num3:
+    print("The largest number is:", num1)
+elif num2 >= num1 and num2 >= num3:
+    print("The largest number is:", num2)
+else:
+    print("The largest number is:", num3)
+print("-------------------------")
+#Example 3 : Check leap year
+year = 2020
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
+    
