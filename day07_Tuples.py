fe5c53f99a5f7630593a8f64578e24ad82ee517f
Prathmesh <uttekarprathmesh30@gmail.com>
2026-02-24 21:27:40 +0530

    Tuples

---
+++ day07_Tuples.py
@@ -0,0 +1,17 @@
+#creating a tuple
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+print("first element:", my_tuple[0])
+print("---------------------------")
+#lopping through a tuple
+print("elements in the tuple:")
+for element in my_tuple:
+    print(element)
+print("---------------------------")
+#length of a tuple
+print("length of the tuple:", len(my_tuple))
+print("---------------------------")
+#count and index of an element in a tuple
+print("count of 2 in the tuple:", my_tuple.count(2))
+print("index of 3 in the tuple:", my_tuple.index(3))
+print("---------------------------")
