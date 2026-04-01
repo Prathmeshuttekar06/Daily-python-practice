7b2ccbfc624ef037493d97046b636234c19bc2b3
Prathmesh <uttekarprathmesh30@gmail.com>
2026-04-01 20:47:18 +0530

    day 10 arrays

---
+++ day10_arrays.py
@@ -0,0 +1,15 @@
+arr1 = [1, 2, 3, 4, 5]
+print("Original array:", arr1)
+# Accessing elements
+print("First element:", arr1[0])
+print("Last element:", arr1[-1])
+# Modifying elements
+arr1[2] = 10
+print("Modified array:", arr1)
+# Adding elements
+arr1.append(6)
+print("Array after appending 6:", arr1)
+# Removing elements
+arr1.remove(2)
+print("Array after removing 2:", arr1)
+
